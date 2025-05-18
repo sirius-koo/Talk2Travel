ff38bb5f288f991eae3c5f9920841acf452b64e3
gomong <kns2931@gmail.com>
2025-05-18 14:32:59 +0900

    chore: remove stray dotenv file

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from talk2travel import create_app
 
 app = create_app()
--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-import os 
-
+import os
 
 class Config:
-    DEBUG = True
+    DEBUG = os.getenv("FLASK_DEBUG", "False") == "True"
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
